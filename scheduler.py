aff538676eb60842dd9fdf817210bb72744614d3
Sergio Pino Márquez <sergiormb88@gmail.com>
2017-06-19 18:39:46 +0200

    Add scheduler day

--- scheduler.py
+++ scheduler.py
@@ -1,16 +1,26 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+from services import TvmazeService
+import telegram
+from telegram.bot import Bot
+import config
+import database
 
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', minutes=1)
-def timed_job():
-    print('This job is run every minute.')
-
-
-@sched.scheduled_job('cron', day=14, hour=15, minute=37)
-def scheduled_job():
-    print('This job is run on day 14 at minute 37, 3pm.')
-
+@sched.scheduled_job('interval', minutes=5)
+def schedule_day():
+    service = TvmazeService()
+    series = service.schedule()
+    bot = Bot(token=config.TOKEN)
+    for serie in series:
+        results = database.get_registers(serie['id'])
+        for result in results:
+            text = serie['name'] + 'HOY'
+            bot.send_message(
+                chat_id=result,
+                text=text,
+                parse_mode=telegram.ParseMode.HTML
+            )
 
 sched.start()
