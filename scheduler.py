17494d9dbff222c02f60af23a50242f9dd0e8ebb
Sergio Pino Márquez <sergiormb88@gmail.com>
2017-06-19 19:54:37 +0200

    Add network with cron

--- scheduler.py
+++ scheduler.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', minutes=1)
+@sched.scheduled_job('cron', hour=12)
 def schedule_day():
     service = TvmazeService()
     episodes = service.schedule()
@@ -23,6 +23,9 @@ def schedule_day():
                 results = database.get_registers(show['id'])
                 for result in results:
                     text = '<b> HOY: %s</b>\n' % show['name']
+                    network = show.get('network', None)
+                    if network:
+                        text += '%s - %s' % (network['name'], network['country']['name'])
                     text = print_episode(text, episode, 'es', 'next_episode')
                     bot.send_message(
                         chat_id=result,
@@ -31,5 +34,8 @@ def schedule_day():
                     )
                     if episode.get('image', None):
                         bot.send_photo(chat_id=result, photo=episode['image'])
+
+if __name__ == '__main__':
+    schedule_day()
 
 sched.start()
