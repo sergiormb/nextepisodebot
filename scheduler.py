a29c192d1e4d99ecffb243c03676df7484e7522f
Sergio Pino Márquez <sergiormb88@gmail.com>
2017-06-19 19:16:44 +0200

    Check is a dict

--- scheduler.py
+++ scheduler.py
@@ -17,19 +17,18 @@ def schedule_day():
     series = service.schedule()
     bot = Bot(token=config.TOKEN)
     for serie in series:
-        results = database.get_registers(serie['id'])
-        for result in results:
-            text = '<b> HOY: </b>'
-            text = print_episode(text, serie, 'es', 'next_episode')
-            bot.send_message(
-                chat_id=result,
-                text=text,
-                parse_mode=telegram.ParseMode.HTML
-            )
-            if serie.get('image', None):
-                bot.send_photo(chat_id=result, photo=serie['image'])
-
-if __name__ == '__main__':
-    schedule_day()
+        if isinstance(serie, dict):
+            if serie.get('id', None):
+                results = database.get_registers(serie['id'])
+                for result in results:
+                    text = '<b> HOY: </b>'
+                    text = print_episode(text, serie, 'es', 'next_episode')
+                    bot.send_message(
+                        chat_id=result,
+                        text=text,
+                        parse_mode=telegram.ParseMode.HTML
+                    )
+                    if serie.get('image', None):
+                        bot.send_photo(chat_id=result, photo=serie['image'])
 
 sched.start()
